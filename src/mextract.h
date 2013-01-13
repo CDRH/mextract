2cb3a10de1d8308335ece4eb20fb309c05f7b63d
Stephen Ramsay <sramsay.unl@gmail.com>
2013-01-12 19:14:02 -0600

    ...

---
+++ src/mextract.h
@@ -0,0 +1,44 @@
+
+/*
+* Assorted macros.
+*
+* This file is part of MorphExtractor (mextract)
+*
+* Written and maintained by Stephen Ramsay <sramsay.unl@gmail.com>
+*
+* Last Modified: Sat Jan 12 19:08:33 CST 2013
+*
+* Copyright (c) 2013 Stephen Ramsay
+*
+* MorphExtractor is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3, or (at your option) any
+* later version.
+*
+* MorphExtractor is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with MorphExtractor; see the file COPYING.  If not see
+* <http://www.gnu.org/licenses/>.
+*/
+
+#define _GNU_SOURCE
+
+#define DEBUG 0
+
+#define Sasprintf(write_to, ...) {                        \
+	int bytes = 0;					  \
+	char *tmp_string_for_extend = (write_to);         \
+	bytes = asprintf(&(write_to), __VA_ARGS__);       \
+	free(tmp_string_for_extend);                      \
+	if (bytes == -1) {			          \
+		printf("Fatal: Unable to assign string"); \
+		exit(EXIT_FAILURE);			  \
+	}						  \
+}
+
+#define debug_print(fmt, ...)					       \
+	do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
